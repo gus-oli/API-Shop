2bc59b10b6d189b72e2f6b2542ae340ffb04f413
Gustavo Oliveira Silva <gustavo.silva1995@outlook.com>
2020-12-07 02:47:06 -0300

    Corrigindo conexão com o banco de dados

--- Startup.cs
+++ Startup.cs
@@ -53,9 +53,9 @@ namespace Shop
                 };
             });
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-            //services.AddDbContext<DataContext>(
-                //opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(
+                opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
                         
             services.AddSwaggerGen(c =>
             {
